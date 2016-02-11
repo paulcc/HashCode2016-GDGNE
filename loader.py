81ff5f275258f0cb4517e4c3cc28d4952676c723
Hugh Kaznowski <hugh@geektalent.com>
2016-02-11 21:34:17 +0000

    marked loader with dictionary@

--- loader.py
+++ loader.py
@@ -2,21 +2,14 @@ def load(filename):
     inputFile = open(filename)
     header = readHeader(inputFile.readline())
     weights = readWeight(inputFile)
-    
+    warehouses = readWarehouse(inputFile)
+    return header, weights, warehouses
+
 
 def readHeader(line):
-    """The first section of the file describes the ​
-     parameters of the simulation​
-      . This section contains a single line 
-      containing the following natural numbers separated by single spaces: 
-      ○ number of rows in the area of the simulation (1 ≤ number of rows ≤ 10000)  
-      ○ number of columns in the area of the simulation (1 ≤ number of columns ≤ 10000)  
-      ○ D​
-       ­ number of drones available (1 ≤ D ≤ 1000)  
-       ○ deadline of the simulation (1 ≤ deadline of the simulation ≤ 1000000)  
-       ○ maximum load of a drone (1 ≤ maximum load of a drone ≤ 10000)  
-       """
-    
+    """
+    Read the header of the input file
+    """
     line = line.split(' ')
     vals = {"rows": int(line[0]),
             "columns": int(line[1]),
@@ -30,36 +23,35 @@ def readWeight(inputFile):
     Read the weight section of the file
     """
     numberOfProducts = int(inputFile.readline())
-    productLine = inputFile.readLine()
+    productLine = inputFile.readline()
     products = productLine.split(' ')
     weights = []
     for i in range(0, len(products)):
-        weights.append(int(products[i]))
+        weights.append({'weight': int(products[i]), 'id': i})
     return weights
-	
+
 def readWarehouse(inputFile):
     """
     Read the warehouse section of the file
     """
     numberOfWarehouses = int(inputFile.readline())
-	warehouses = {}
-	warehouseProducts = {}
-	for w in numberOfWarehouses:
-		warehouseCoordLine = inputFile.readLine()
-		warehouseCoords = warehouseCoordLine.split(' ')
-		x = int(warehouseCoords[0])
-		y = int(warehouseCoords[1])
-		warehouses[w] = (x, y)
-		
-		productLine = inputFile.readLine()
-		products = productLine.split(' ')
-	    productsStored = []
-		for i in range(0, len(products)):
-			productsStored.append(int(products[i]))
-		
-		warehouseProducts[w] = (productsStored)
-		return warehouses, warehouseProducts
+    warehouses = []
+    warehouseProducts = {}
+    for w in range(0, numberOfWarehouses):
+        warehouseCoordLine = inputFile.readline()
+        warehouseCoords = warehouseCoordLine.split(' ')
+        coords = {'x': int(warehouseCoords[0]), 'y': int(warehouseCoords[1])}
+        productLine = inputFile.readline()
+        products = productLine.split(' ')
+        productsStored = []
+        for i in range(0, len(products)):
+            productsStored.append(int(products[i]))
+        warehouses.append( {'pos': coords, 'prods': productsStored})
+   
+    return warehouses
 
 
 
-
+if __name__ == "__main__":
+    #print load("busy_day.in")
+    print load("mother_of_all_warehouses.in")

d641aecb6485ac48587bf0cd18e0647440e49af4
Hugh Kaznowski <hugh@geektalent.com>
2016-02-11 19:48:58 +0000

    Drone with helpers, working on warehouse now

--- drone.py
+++ drone.py
@@ -8,7 +8,7 @@ class Drone:
         self.maxWeight = maxWeight
         self.coordX = coordX
         self.coordY = coordY
-        self.action=False
+        self.action=None
         self.products={}
         self.targetX = coordX
         self.targetY = coordY
@@ -20,8 +20,8 @@ class Drone:
         The drone checks if it can make that command and then stacks it
         The drone performs its action with execute
         """
-        if self.action==False:
-            self.action=True
+        if self.action==None:
+            self.action="load"
             self.targetX=warX
             self.targetY=warY
     
@@ -29,8 +29,8 @@ class Drone:
         """
         Tell the drone to go to the destination (dstX, dstY) and 
         """
-        if self.action==False:
-            self.action=True
+        if self.action==None:
+            self.action="deliver"
             self.targetX=dstX
             self.targetY=dstY
 
@@ -39,27 +39,50 @@ class Drone:
         """
         Perform the planned action
         """
-        if self.action==True:
+        if self.action != None:
             if self.targetX != self.coordX:
                 distanceX = self.targetX-self.coordX
                 directionX = distanceX/abs(distanceX)
-                self.coordX+=directionX
+                self.coordX += directionX
             elif self.targetY != self.coordY:
                 distanceY = self.targetY - self.coordY
                 directionY = distanceY/abs(distanceY)
-                self.coordY+=directionY
+                self.coordY += directionY
             else:
                 raise Exception("This condition should not happen")
             matchesX = self.coordX == self.targetX
             matchesY = self.coordY == self.targetY
             if (matchesX and matchesY):
                 # Reached destination
-                self.action=False
+                if self.action=="load":
+                    #TODO load correct number of product
+
+                elif self.action=="deliver":
+                    #TODO drop off correct number of product
+                else:
+
+                self.action=None
 
     def coord(self):
+        """
+        Return the coordinates of the drone
+        """
         return (self.coordX, self.coordY)
 
+    def turns(self):
+        """
+        Calculate the number of turns remaining to complete action
+        """
+        distX = abs(self.targetX-self.coordX)
+        distY = abs(self.targetY-self.coordY)
+        return distX+distY
+
     def target(self):
+        """
+        Return the coordinates of the target
+        """
         return (self.targetX, self.targetY)
 
+    def __str__(self):
+        return "Drone(action=%s, coord=(%d, %d), target=(%d, %d), distance=%d)" % (self.action, self.coordX, self.coordY, self.targetX, self.targetY, self.turns())
 

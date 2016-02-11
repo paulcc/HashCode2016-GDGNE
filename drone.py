dcb8b38a042e427a7218b5e0bd009019c34b9c29
Hugh Kaznowski <hugh@geektalent.com>
2016-02-11 19:15:58 +0000

    Added drone code

---
+++ drone.py
@@ -0,0 +1,51 @@
+class Drone:
+    
+    def __init__(self, maxWeight, coordX, coordY):
+        """
+        Create a drone with maxweight and position
+        """
+        self.weight = 0
+        self.maxWeight = maxWeight
+        self.coordX = coordX
+        self.coordY = coordY
+        self.action=False
+        self.products={}
+
+    def load(self, warX, warY, productType, productCount):
+        """
+        Tell the drone to go to warehouse (warX, warY)
+        The drone checks if it can make that command and then stacks it
+        The drone performs its action with execute
+        """
+        if self.action==False:
+            self.action=True
+            self.targetX=warX
+            self.targetY=warY
+    
+    def deliver(self, dstX, dstY, productIndex, productCount):
+        """
+        Tell the drone to go to the destination (dstX, dstY) and 
+        """
+
+    def execute(self):
+        """
+        Perform the planned action
+        """
+        if self.action==True:
+            if self.targetX != self.coordX:
+                distanceX = self.targetX-self.coordX
+                directionX = distanceX/abs(distanceX)
+                self.coordX+=directionX
+            elif self.targetY != self.coordY:
+                distanceY = self.targetY - self.coordY
+                directionY = distanceY/abs(distanceY)
+                self.coordY+=directionY
+            else:
+                except Exception("This condition should not happen")
+            matchesX = self.coordX == self.targetX
+            matchesY = self.coordY == self.targetY
+            if (matchesX and matchesY):
+                # Reached destination
+                self.action=False
+
+

6ab8b99b36f6ede8acb097c809aacd538e1b9dce
Ayman Osman <aymano.osman@gmail.com>
2016-02-11 21:26:40 +0000

    wip

--- haskell/Foo.hs
+++ haskell/Foo.hs
@@ -1,23 +1,40 @@
+import Data.List.Split
 import Data.Array
+import qualified Data.Map as Map
 
 main :: IO ()
 main = run >>= print
 
-run :: IO (Params, Int, [Int])
+-- run :: IO (Params, Int, [Int])
 run =
   do f <- lines <$> readFile "../mother_of_all_warehouses.in"
      let
-         (params':numprodtypes':weights':f1) = f
+         (params':numprodtypes':weights':numWares':f1) = f
          [a,b,c,d,e] = read <$> words params' :: [Int]
          params = Params a b c d e
          numProdTypes = read numprodtypes' :: Int
          weights = read <$> words weights' :: [Int] -- assert lenght w == numP
-     return (params, numProdTypes, weights)
+         numWares = read numWares' :: Int
+         wares' = chunksOf 2 $ take (2*numWares) f1
+         wares = map mkWare wares' :: [Warehouse]
+     {- <$>
 
+## ware hoisees and avail of prods
 
-lb, ub :: Int
-lb = 0
-ub = 9
+- W numWares <10,000
+2- per warehouses
+
+  -}
+     return (params, numProdTypes, weights, wares)
+
+mkWare :: [String] -> Warehouse
+mkWare [coord', prodFoo'] =
+  let [x, y] = read <$> words coord' :: [Int]
+      prodFoo = read <$> words prodFoo' :: [Int]
+      stock = Map.fromAscList $ zip [0..] prodFoo
+  in
+    Warehouse 99 (x, y) stock
+mkWare _ = error "Bork! Expected two length list"
 
 data Params = Params
   { numRows :: Int -- 1 to 10,000
@@ -27,17 +44,23 @@ data Params = Params
   , maxLoad :: Int -- 1 to 10,000
   } deriving (Show)
 
-grid :: Array Int Cell
-grid =
-  listArray (lb, ub) (repeat emptyCell)
-
-data Cell = Cell
-  { cellWare :: Maybe Warehouse
-  } deriving (Show)
-
-emptyCell :: Cell
-emptyCell = Cell Nothing
-
 data Warehouse = Warehouse
  { wareId :: Int
+ , wareCoord :: (Int, Int)
+ , wareProdStock :: Map.Map Int Int -- prod stuff
  } deriving (Show)
+
+-- lb, ub :: Int
+-- lb = 0
+-- ub = 9
+
+-- grid :: Array Int Cell
+-- grid =
+--   listArray (lb, ub) (repeat emptyCell)
+
+-- data Cell = Cell
+--   { cellWare :: Maybe Warehouse
+--   } deriving (Show)
+
+-- emptyCell :: Cell
+-- emptyCell = Cell Nothing

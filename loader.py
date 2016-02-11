961de8651fbcb625f0c99c6b18d1589e4f79d53f
jameswhinn <jameswhinn89@gmail.com>
2016-02-11 20:40:13 +0000

    lolwut

--- loader.py
+++ loader.py
@@ -36,6 +36,29 @@ def readWeight(inputFile):
     for i in range(0, len(products)):
         weights.append(int(products[i]))
     return weights
+	
+def readWarehouse(inputFile):
+    """
+    Read the warehouse section of the file
+    """
+    numberOfWarehouses = int(inputFile.readline())
+	warehouses = {}
+	warehouseProducts = {}
+	for w in numberOfWarehouses:
+		warehouseCoordLine = inputFile.readLine()
+		warehouseCoords = warehouseCoordLine.split(' ')
+		x = int(warehouseCoords[0])
+		y = int(warehouseCoords[1])
+		warehouses[w] = (x, y)
+		
+		productLine = inputFile.readLine()
+		products = productLine.split(' ')
+	    productsStored = []
+		for i in range(0, len(products)):
+			productsStored.append(int(products[i]))
+		
+		warehouseProducts[w] = (productsStored)
+		return warehouses, warehouseProducts
 
 
 
